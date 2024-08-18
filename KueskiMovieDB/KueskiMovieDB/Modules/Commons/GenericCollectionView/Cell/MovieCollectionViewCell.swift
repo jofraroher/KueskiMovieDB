6500f08e6a90f3f5b008ef16a5e74576487a85b8
Francisco Rosales <jofraroher@gmail.com>
2024-08-17 18:23:33 -0600

    feat: create generic collection view cell

---
+++ KueskiMovieDB/KueskiMovieDB/Modules/Commons/GenericCollectionView/Cell/MovieCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCollectionViewCell.swift
+//  KueskiMovieDB
+//
+//  Created by Francisco Rosales on 17/08/24.
+//
+
+import UIKit
+
+final class MovieCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- KueskiMovieDB/KueskiMovieDB/Modules/Movies/View/MoviesViewController.swift
+++ KueskiMovieDB/KueskiMovieDB/Modules/Movies/View/MoviesViewController.swift
@@ -9,7 +9,40 @@ import UIKit
 
 class MoviesViewController: UIViewController {
 
+    private let movieItems = Array(repeating: "TEST", count: 5)
+    private var reusableCollectionView: GenericCollectionView<String, MovieCollectionViewCell>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        let frame = adjustedFrame()
+        setupMoviesCollestionView(with: frame)
+    }
+    
+    private func setupMoviesCollestionView(with frame: CGRect) {
+        let collectionView = GenericCollectionView<String, MovieCollectionViewCell>(
+            frame: frame,
+            items: movieItems,
+            configureCell: { _, _ in
+                print("Configure cell")
+            }, didSelectItem: { _ in
+                print("Item selected")
+            }
+        )
+        reusableCollectionView = collectionView
+        if let collectionView = reusableCollectionView {
+            view.addSubview(collectionView)
+        }
+    }
+    
+    private func adjustedFrame() -> CGRect {
+        return view.bounds.insetBy(
+            dx: 0,
+            dy: tabBarController?.tabBar.frame.height ?? 0
+        )
     }
 }
