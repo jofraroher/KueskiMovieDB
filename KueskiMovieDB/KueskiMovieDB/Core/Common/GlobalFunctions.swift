4056ad86d2e211ab9109e43ebb5620372cb72d50
Francisco Rosales <jofraroher@gmail.com>
2024-08-18 00:42:51 -0600

    feat: creating a router to get the movies now playing

---
+++ KueskiMovieDB/KueskiMovieDB/Core/Common/GlobalFunctions.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalFunctions.swift
+//  KueskiMovieDB
+//
+//  Created by Francisco Rosales on 18/08/24.
+//
+
+enum StringTable: String {
+    case KueskiMovieDBConfiguration
+}
+
+enum ResourceType: String {
+    case Configuration
+}
+
+enum FileType: String {
+    case plist
+}
