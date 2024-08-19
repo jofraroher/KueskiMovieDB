fdca45b422673d57d1f22f87347e2b6760d4f9a2
Francisco Rosales <jofraroher@gmail.com>
2024-08-19 07:22:39 -0600

    test: add unit test for movie storage service

--- KueskiMovieDB/KueskiMovieDB/Modules/Movies/Utils/MovieSyncService.swift
+++ KueskiMovieDB/KueskiMovieDB/Modules/Movies/Utils/MovieSyncService.swift
@@ -12,7 +12,6 @@ protocol MovieSyncServiceProtocol {
 final class MovieSyncService: MovieSyncServiceProtocol {
     func syncWithSavedMovies(newItems: [Movie], storageService: MovieStorageServiceProtocol) async throws -> [Movie] {
         let savedItems = try await storageService.getSavedMovies()
-        print(savedItems)
         let savedItemIDs = Set(savedItems.map { $0.id })
         
         let updatedItems = newItems.map { movie -> Movie in
---
+++ KueskiMovieDB/KueskiMovieDBTests/CommonMocks/MockNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  MockNavigationController.swift
+//  KueskiMovieDBTests
+//
+//  Created by Francisco Rosales on 19/08/24.
+//
+
+import UIKit
+
+final class MockNavigationController: UINavigationController {
+    var pushViewControllerCalled = false
+    var pushedViewController: UIViewController?
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        pushViewControllerCalled = true
+        pushedViewController = viewController
+        super.pushViewController(viewController, animated: animated)
+    }
+}
